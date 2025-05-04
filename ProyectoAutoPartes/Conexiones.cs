699feca6f06e7eb5a149fee82fea8730c4400fdd
Daniel López <lopezgelber769@gmail.com>
2025-05-04 13:17:22 -0600

    va la clase de la conexion y el app.confi de eso

---
+++ ProyectoAutoPartes/Conexiones.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        // Leer la cadena de conexión desde App.config
+        string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        try
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("¡Conexión exitosa!");
+
+                // Ejemplo: Consulta SQL
+                string query = "SELECT * FROM usuarios";
+                var command = new MySqlCommand(query, connection);
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader["nombre"].ToString());
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
